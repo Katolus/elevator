930d579be17d470afae23899877347f4d7f05a1e
Katolus <pkatolik@healthshare.com.au>
2020-09-26 13:03:40 +1000

    Add a door status

--- elevator/elevator.py
+++ elevator/elevator.py
@@ -4,18 +4,28 @@ class InsideBoard:
     """
 
 
+class Door:
+    OPEN = "O"
+    CLOSED = "C"
+
+
 class Elevator:
     def __init__(self, lowest_floor: int, highest_floor: int) -> None:
         self.LOWEST_FLOOR = lowest_floor
         self.HIGHEST_FLOOR = highest_floor
+        self.door = Door.CLOSED
         self.current_floor = 0  # Starts from ground level
 
     def _up_1(self):
+        if self.door != Door.CLOSED:
+            raise ValueError("Cannot move the elevator if the door is not closed")
         if self.current_floor + 1 > self.HIGHEST_FLOOR:
             raise ValueError(f"Can't go higher that the {self.HIGHEST_FLOOR} floor")
         self.current_floor += 1
 
     def _down_1(self):
+        if self.door != Door.CLOSED:
+            raise ValueError("Cannot move the elevator if the door is not closed")
         if self.current_floor - 1 < self.LOWEST_FLOOR:
             raise ValueError(f"Can't go lower that the {self.LOWEST_FLOOR} floor")
         self.current_floor -= 1
--- tests/test_elevator.py
+++ tests/test_elevator.py
@@ -1,6 +1,7 @@
 import pytest
 
 from elevator.elevator import Elevator
+from elevator.elevator import Door
 
 
 def test_cannot_go_above_highest_floor():
@@ -9,6 +10,21 @@ def test_cannot_go_above_highest_floor():
     with pytest.raises(ValueError) as error:
         elevator._up_1()
         assert str(error) == "Can't go higher that the 1"
+
+def test_cannot_move_if_doors_are_not_closed():
+    elevator = Elevator(-2, 2)
+    elevator._down_1()
+    elevator._up_1()
+    # Open door
+    elevator.door = Door.OPEN
+    with pytest.raises(ValueError) as error:
+        elevator._down_1()
+    with pytest.raises(ValueError) as error:
+        elevator._up_1()
+    # Close door 
+    elevator.door = Door.CLOSED
+    elevator._down_1()
+    elevator._up_1()
 
 def test_cannot_go_below_lowest_floor():
     elevator = Elevator(0, 1)
